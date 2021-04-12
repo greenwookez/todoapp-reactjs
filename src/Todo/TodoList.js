47f683bcf50830cf5cceb33865abe3fc9f215b57
greenwookez <greenwookez@gmail.com>
2021-04-12 14:19:49 +0300

    Init commit

---
+++ src/Todo/TodoList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import TodoItem from "./TodoItem";
+
+const styles = {
+  ul: {
+    listStyle: "none",
+    margin: 0,
+    padding: 0,
+  },
+};
+function TodoList(props) {
+  return (
+    <ul style={styles.ul}>
+      {props.todos.map((item, index) => {
+        return (
+          <TodoItem
+            todo={item}
+            key={item.id}
+            index={index + 1}
+            onChange={props.onToggle}
+          />
+        );
+      })}
+    </ul>
+  );
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onToggle: PropTypes.func.isRequired,
+};
+
+export default TodoList;
